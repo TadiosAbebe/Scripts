c3b37ccce799fc27bde5ab3dc6bba75c6cff1d9e
tadios <besttadios@gmail.com>
2022-06-23 20:31:06 +0300

    Added Batch Icon Changer.ps1 script

---
+++ Powershell Scripts/Batch Icon Changer.ps1
@@ -0,0 +1,18 @@
+﻿Get-ChildItem -Directory | ForEach-Object {
+
+Set-Location $_.Name
+
+$TargetDirectory = Get-Location
+
+$DesktopIni = @"
+[.shellClassInfo]
+IconResource = $TargetDirectory\icon.ico,0
+"@
+
+Add-Content "$($TargetDirectory)\desktop.ini" -Value $DesktopIni
+(Get-Item "$($TargetDirectory)\desktop.ini" -Force).Attributes = 'Hidden, System, Archive'
+(Get-Item $TargetDirectory -Force).Attributes = 'ReadOnly, Directory'
+Write-Host "Completed"
+Set-Location ..
+}
+pause
--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Bunch of scripts that I used to automate my workflow.
 
 - it looks for a list.txt file inside the directory that the script is run in
 - it iterates through each line in that text file and creates an empty file with the corresponding line as its filename
+
+## Batch Icon Changer.ps1
+
+- this script changes the folder icon of Folders under a certain directory to icon.ico file specified in each directory accordingly.
